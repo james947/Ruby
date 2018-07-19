74c44378d0001d27396da46404c9a0dec0cf8fe8
james947 <jmuriuki947@gmail.com>
2018-07-19 09:52:57 +0300

    Add dictionaries

---
+++ hashes.rb
@@ -0,0 +1,12 @@
+#hashes are dictionaries
+
+states = {
+    "Kenya" => "Nairobi",
+    "Uganda" => "Kampala",
+    "Tanzania" => "Dodoma",
+    :Sudan => "Khartoum"
+
+}
+
+puts states["Kenya"]
+puts states[:Sudan]
