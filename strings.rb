3f54da77240aa21cafd5aa5ba4bf19a4e00c51ae
james947 <jmuriuki947@gmail.com>
2018-07-19 09:53:37 +0300

    Add use of strings

---
+++ strings.rb
@@ -0,0 +1,8 @@
+character_name = "   james    "
+
+puts character_name.upcase()
+
+puts character_name.downcase()
+
+puts character_name.strip() # removes white spaces
+puts character_name.include? "james" # checks if true
