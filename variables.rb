423adcfe149ee6ae1da8dd913653f0e48e301e45
james947 <jmuriuki947@gmail.com>
2018-07-19 09:53:47 +0300

    Add use of variabkes

---
+++ variables.rb
@@ -0,0 +1,15 @@
+character_name = "james"
+character_age = 35
+
+puts( "my name is " + character_name)
+puts ("my age is " + character_age.to_s)
+
+books = {}
+
+# hash / dictionary
+# value and key
+books["kidagaa"] = :awesome 
+ 
+puts books
+
+print character_name.upcase()
