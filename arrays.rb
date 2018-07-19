3b8fe57f6ab2a9f5e2a1d150fefcc6d5641ecbe0
james947 <jmuriuki947@gmail.com>
2018-07-19 09:52:39 +0300

    Add array use cases

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+friends = ["lisa", "james", "vick"]
+
+puts friends
+print( "we are printing index zero " + friends[0])
+puts()
+print( "we are printing index negative zero " + friends[-1])  
+puts()
+puts friends[0, 2] #stops printing from index 2
+
+enemies = Array.new # initializes an array /list
+
+enemies[0] = "none"
+
+puts enemies
+
